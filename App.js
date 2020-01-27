4da62492fdae476152ad9fa9aa3365c00e0083a7
Deulect <deulect@Illyas.local>
2020-01-27 19:03:16 +0530

    Basic Animation while dragging

--- App.js
+++ App.js
@@ -1,16 +1,81 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import {PanGestureHandler, State} from 'react-native-gesture-handler';
+import Animated from 'react-native-reanimated';
+
+const {
+  set,
+  cond,
+  eq,
+  spring,
+  startClock,
+  stopClock,
+  clockRunning,
+  defined,
+  Value,
+  Clock,
+  event,
+} = Animated;
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {};
+    this.translateX = new Value(0);
+    const dragX = new Value(0);
+    const state = new Value(-1); // -1 means state in undetermined
+    // since we imported from Animated above we can use directly
+
+    this.onGestureEvent = Animated.event([
+      {
+        nativeEvent: {
+          // importent pont here here is translation X
+          translationX: dragX,
+          state: state,
+        },
+      },
+    ]);
+    const transX = new Value();
+    this.translateX = cond(
+      eq(state, State.ACTIVE),
+      [
+        set(transX, dragX), // set the value of dragX to other
+        transX, // return this value then
+        //if state active this block will run
+        //when state is active we want to calculate new panValue
+      ],
+      [
+        set(transX, 0),
+        transX,
+        //if inactive this block will run
+      ],
+    );
+    //above
+    //i- condition
+    //ii-if true this will run
+    //iii-otherwise 3 will run
+    //condition we want to check is this state is active
   }
+
+  //when the circle is dragged the curren state become active
+  //once box released the oldstate become active
+  //current state becomes end
 
   render() {
     return (
       <View style={styles.main}>
-        <Text> App </Text>
+        <PanGestureHandler
+          onGestureEvent={this.onGestureEvent}
+          onHandlerStateChange={this.onGestureEvent}>
+          <Animated.View
+            style={[
+              styles.box,
+              {
+                transform: [{translateX: this.translateX}],
+                // transform is used for styyling
+              },
+            ]}></Animated.View>
+        </PanGestureHandler>
       </View>
     );
   }
@@ -18,9 +83,16 @@ class App extends Component {
 const styles = StyleSheet.create({
   main: {
     flex: 1,
-    backgroundColor: 'grey',
-    paddingTop: '20%',
+    backgroundColor: '#fff',
     alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  box: {
+    height: 100,
+    width: 100,
+    backgroundColor: 'red',
+    borderRadius: 50,
   },
 });
 
---
+++ notes.js
@@ -0,0 +1,3 @@
+// do pod install after installing the packages ie gusture handler and reanimated
+// rebuild tha app
+//
