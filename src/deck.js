912f5c481b9877e8e77a0ce6b90b2d8b89315e76
Illyas <illyas.deulect@gmail.com>
2020-05-09 15:34:41 +0530

    panResponder basics added

--- src/deck.js
+++ src/deck.js
@@ -1,20 +1,37 @@
 import React, {Component} from 'react';
-import {View, Text, Animated, StyleSheet} from 'react-native';
+import {View, Text, Animated, StyleSheet, PanResponder} from 'react-native';
 
 class Deck extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    const position = new Animated.ValueXY();
+    //  1- create the panResponder
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (e, gestureState) => {
+        position.setValue({
+          x: gestureState.dx,
+          y: gestureState.dy,
+        });
+      },
+      onPanResponderRelease: (e, gestureState) => {},
+    });
+    this.state = {panResponder, position}; // using this we can refrence from inside render and component
   }
   renderCards = () => {
-    console.warn('aaaa');
     return this.props.data.map(item => {
       return this.props.renderCard(item);
     });
   };
 
   render() {
-    return <View>{this.renderCards()}</View>;
+    return (
+      <Animated.View
+        style={this.state.position.getLayout()}
+        {...this.state.panResponder.panHandlers}>
+        {this.renderCards()}
+      </Animated.View>
+    );
   }
 }
 
