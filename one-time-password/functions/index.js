876bf6ebc4569b776f87424929b849d2c9dd8493
Illyas <illyas.deulect@gmail.com>
2020-05-12 13:08:02 +0530

    Added verify token functionality

--- one-time-password/functions/index.js
+++ one-time-password/functions/index.js
@@ -2,6 +2,7 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin'); //this is an access to our firebase project, ie our service account
 const createUser = require('./create_user');
 const requestOneTimePassword = require('./request-one-time-password');
+const verifyOneTimePassword = require('./verify-one-time-password');
 
 const serviceAccount = require('./service_account.json');
 
@@ -13,6 +14,9 @@ admin.initializeApp({
 exports.createUser = functions.https.onRequest(createUser);
 exports.requestOneTimePassword = functions.https.onRequest(
   requestOneTimePassword,
+);
+exports.verifyOneTimePassword = functions.https.onRequest(
+  verifyOneTimePassword,
 );
 //this means any time an https request comes in run the command above, ie createUser
 //this is google cloud function we defined
